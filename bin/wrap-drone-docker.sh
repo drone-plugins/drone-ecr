24d027b851c8817fe35a9b5290a1edbcc564aded
Michael J. Schultz <mjschultz@gmail.com>
2017-01-25 02:43:54 +0000

    Support registry-ids parameter for ECR

--- bin/wrap-drone-docker.sh
+++ bin/wrap-drone-docker.sh
@@ -5,6 +5,7 @@
 [ -n "$ECR_ACCESS_KEY" ] && export PLUGIN_ACCESS_KEY=${ECR_ACCESS_KEY}
 [ -n "$ECR_SECRET_KEY" ] && export PLUGIN_SECRET_KEY=${ECR_SECRET_KEY}
 [ -n "$ECR_CREATE_REPOSITORY" ] && export PLUGIN_SECRET_KEY=${PLUGIN_CREATE_REPOSITORY}
+[ -n "$ECR_REGISTRY_IDS" ] && export PLUGIN_REGISTRY_IDS=${ECR_REGISTRY_IDS}
 
 # set the region
 export AWS_DEFAULT_REGION=${PLUGIN_REGION:-'us-east-1'}
@@ -14,8 +15,11 @@ if [ -n "$PLUGIN_ACCESS_KEY" ] && [ -n "$PLUGIN_SECRET_KEY" ]; then
   export AWS_SECRET_ACCESS_KEY=${PLUGIN_SECRET_KEY}
 fi
 
+# support --registry-ids if provided
+[ -n "$PLUGIN_REGISTRY_IDS" ] && export REGISTRY_IDS="--registry-ids ${PLUGIN_REGISTRY_IDS}"
+
 # get token from aws
-aws_auth=$(aws ecr get-authorization-token --output text)
+aws_auth=$(aws ecr get-authorization-token --output text ${REGISTRY_IDS:-''})
 
 # map some ecr specific variable names to their docker equivalents
 export DOCKER_USERNAME=AWS
